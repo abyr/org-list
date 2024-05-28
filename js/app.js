df599204d6e3f8d4bf8ec18b68567bffc0e137c8
Oleksandr Denysenko <aleksandr.denisenko.it@gmail.com>
2024-05-28 15:09:51 +0000

    add and show notes

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
 import LayoutView from './layout-view.js';
 
-const onWindowLoad = () => {
+const onWindowLoad = async () => {
     console.log('onload');
 
     const appEl = document.querySelector('#main-container');
 
     const layout = new LayoutView({ element: appEl });
 
-    layout.render();
+    await layout.init();
+
+    layout.asyncRender();
 };
 
 window.addEventListener("load", onWindowLoad);
--- js/layout-view.js
+++ js/layout-view.js
@@ -1,13 +1,72 @@
-import View from './classes/view.js';
+import AsyncView from './classes/async-view.js';
+import NotesStoreAdapter from './storage-adapters/notes-adapter.js';
+import ListsStoreAdapter from './storage-adapters/lists-adapter.js';
 
-class LayoutView extends View {
+class LayoutView extends AsyncView {
 
-    getHtml() {
-        const len = 0;
+    async init() {
+        this.notesAdapter = new NotesStoreAdapter();
+        this.listsAdapter = new ListsStoreAdapter();
+
+        await this.notesAdapter.connect();
+        await this.listsAdapter.connect();
+    }
+
+    async asyncRender() {
+        const html = await this.getAsyncHtml();
+
+        this.element.innerHTML = html;
+
+        const addNoteEl = document.querySelector('#add-note-input');
+
+        addNoteEl.addEventListener('keydown', this.addNote.bind(this));
+    }
+
+    async getAsyncHtml() {
+        const notes = await this.getNotes();
+
+        const notesHtml = notes.map(x => {
+            return `
+                <div class="note" data-id="${x.id}">
+                    <b>${x.title}</b> created at ${new Date(x.createdAt).toLocaleString()}
+                </div>
+            `;
+        }).join('');
+
+        const addNotesHtml = `
+            <div>
+                <input id="add-note-input" type="text" placeholder="Add a note..." ></input>
+            </div>
+        `;
 
         return `
-            Have ${len} notes.
+            ${addNotesHtml}
+
+            Have ${notes.length} notes.
+
+            ${notesHtml}
         `;
+    }
+
+    async getNotes() {
+        return await this.notesAdapter.getAll();
+    }
+
+    async addNote(evnt) {
+        if (evnt.key !== "Enter") {
+            return;
+        }
+
+        evnt.preventDefault();
+
+        const el = evnt.currentTarget;
+        const text = el.value.trim();
+
+        if (text) {
+            await this.notesAdapter.put(null, {
+                title: text,
+            });
+        }
     }
 }
 

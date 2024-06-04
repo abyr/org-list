56a2736fdfcde07620a367dabb22b0cc9ac0b1a5
Oleksandr Denysenko <aleksandr.denisenko.it@gmail.com>
2024-06-04 16:13:21 +0300

    add events sub and unsub methods

--- js/classes/view.js
+++ js/classes/view.js
@@ -9,6 +9,8 @@ class View {
     constructor({ element }) {
         this.element = element;
         this.uid = this.getUid();
+
+        this.eventSubscribersMap = {};
     }
 
     getUid() {
@@ -29,13 +31,51 @@ class View {
 
     destroy() {
         this.cleanup();
+        this.eventSubscribersMap = null;
     }
 
     cleanup() {
+        this.unsubscribeAllEvents();
+
         while(this.element.firstChild) {
             this.element.removeChild(this.element.firstChild);
         }
     }
+
+    subscribeElementEvent(element, eventName, subscriber) {
+        if (!element || !subscriber || !eventName) {
+            return;
+        }
+
+        if (!this.eventSubscribersMap[eventName]) {
+            this.eventSubscribersMap[eventName] = [];
+        }
+
+        this.eventSubscribersMap[eventName].push({ element, subscriber });
+
+        element.addEventListener(eventName, subscriber, false);
+    }
+
+    unsubscribeAllEvents() {
+        if (!this.eventSubscribersMap) {
+            return;
+        }
+
+        const eventNames = Object.keys(this.eventSubscribersMap);
+
+        eventNames.forEach(eventName => {
+            const eventSubscribers = this.eventSubscribersMap[eventName];
+
+            eventSubscribers.forEach(({ element, subscriber }) => {
+                element.removeEventListener(eventName, subscriber, false);
+            });
+
+            this.eventSubscribersMap[eventName] = null;
+        });
+
+        this.eventSubscribersMap = {};
+    }
+
 }
 
 export default View;

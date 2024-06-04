b06ffd3425bb17b3095c573a07e512c0d1917a95
Oleksandr Denysenko <aleksandr.denisenko.it@gmail.com>
2024-06-04 10:11:26 +0300

    extract tags view

---
+++ js/views/tags-view.js
@@ -0,0 +1,46 @@
+import View from '../classes/view.js';
+import messageBus from '../classes/shared-message-bus.js';
+
+class TagsView extends View {
+
+    render() {
+        super.render();
+
+        const tagsEl = this.element.querySelectorAll('.tag-btn');
+
+        tagsEl.forEach(el => el.addEventListener('click', this.filterTag.bind(this) ));
+    }
+
+    getHtml() {
+        const tags = this.getTags();
+
+        return `
+            <ul class="box tags-box list">
+                ${tags.map(tag => {
+                    return `
+                                <li class="tags-item tag">
+                                    <button class="tag-btn" data-tag="${tag}">${tag}</button>
+                                </li>
+                            `;
+                }).join('')}
+            </ul>
+        `;
+    }
+
+    filterTag(event) {
+        const el = event.currentTarget;
+        const tag = el.innerText;
+
+        messageBus.publish('tag:selected', { tag });
+    }
+
+    setTags(tags) {
+        this.tags = tags;
+    }
+
+    getTags() {
+        return this.tags;
+    }
+}
+
+export default TagsView;

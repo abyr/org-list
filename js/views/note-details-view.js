fc4c66deecd15aab27193bbb651a12123d41e6c3
Oleksandr Denysenko <aleksandr.denisenko.it@gmail.com>
2024-06-20 10:07:38 +0300

    add list selector

--- js/views/note-details-view.js
+++ js/views/note-details-view.js
@@ -1,5 +1,6 @@
 import View from '../classes/view.js';
 import notesRepository from '../storage/notes-repository.js';
+import listsRepository from '../storage/lists-repository.js';
 import messageBus from '../classes/shared-message-bus.js';
 
 class NoteDetailsView extends View {
@@ -13,13 +14,44 @@ class NoteDetailsView extends View {
                 contenteditable="true"
                 aria-placeholder="title"
                 aria-labelledby="noteTitleLabel">${note.title}</div>
-        `
+                
+            <div class="box-v16" id="select-list-box-${note.id}">
+                <label for="select-list-for-${this.note.id}">List</label>
+                
+                <select id="select-list-for-${this.note.id}">
+                    <option value="">Unlisted</option>
+                </select>
+            </div>
+        `;
     }
 
-    applyEvents() {
+    async applyEvents() {
         const titleEl = this.element.querySelector('#note-title-' + this.note.id);
 
         this.subscribeElementEvent(titleEl, 'blur', this.applyTitle.bind(this));
+
+        const selectListEl = this.element.querySelector(`#select-list-for-${this.note.id}`);
+        this.subscribeElementEvent(selectListEl, 'change', this.applyList.bind(this));
+
+    }
+
+    async postRender() {
+        const lists = await listsRepository.getAll();
+        const listId = this.list && this.list.id;
+
+        const selectListEl = this.element.querySelector(`#select-list-for-${this.note.id}`);
+
+        const selectHtml = `
+            <option value="" ${this.list ? '' : 'selected' }>Unlisted</option>
+        
+            ${lists.map(list => {
+                return `<option value="${list.id}" 
+                            ${listId && list.id === listId ? 'selected' : '' }
+                        >${list.title}</option>`;
+            }).join('')}
+        `;
+
+        selectListEl.innerHTML = selectHtml;
     }
 
     async applyTitle(event) {
@@ -38,9 +70,50 @@ class NoteDetailsView extends View {
         });
     }
 
-    setNote(note) {
-        this.note = note;
+    async applyList(event) {
+        const listId = event.currentTarget.value;
+
+        if (!listId) {
+            return await this.removeFromList();
+        }
+
+        const list = await listsRepository.get(listId);
+
+        if (list.notes) {
+            if (list.notes.includes(this.note.id)) {
+                return
+            }
+
+            list.notes.push(this.note.id);
+        } else {
+            list.notes = [this.note.id];
+        }
+
+        await listsRepository.update(listId, list);
+
+        console.log('apply list', list);
     }
+
+    async removeFromList() {
+        const index = this.list.notes.indexOf(this.note.id);
+
+        if (index > -1) {
+            this.list.notes.splice(index, 1);
+
+            await listsRepository.update(this.list.id, this.list);
+        }
+    }
+
+    async setNote(note) {
+        this.note = note;
+
+        const lists = await listsRepository.getAll();
+
+        const list = lists.find(x => x.notes && x.notes.includes(note.id));
+
+        this.list = list || {};
+    }
+
 }
 
 export default NoteDetailsView;

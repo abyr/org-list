7481de95e281b7c14c23bfc1a7e7b9bb8a165251
Oleksandr Denysenko <aleksandr.denisenko.it@gmail.com>
2024-07-03 12:10:36 +0300

    add shared static list

---
+++ js/storage/static-lists.js
@@ -0,0 +1,13 @@
+const staticLists = [{
+    icon: '&#10064;',
+    id: 'inbox',
+    title: 'Inbox',
+    notes: [],
+}, {
+    icon: '&star;',
+    id: 'starred',
+    title: 'Starred',
+    notes: [],
+}];
+
+export default staticLists;

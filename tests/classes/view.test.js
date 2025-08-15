2424e9437ba9e91c1a204077bd1cbfcdbbd21677
Oleksandr Denysenko <aleksandr.denisenko.it@gmail.com>
2025-08-15 09:23:57 +0300

    add tests

--- tests/classes/view.test.js
+++ tests/classes/view.test.js
@@ -1,5 +1,6 @@
 import { describe, it, expect, should, beforeEach, vi } from 'vitest';
 import View from '../../js/classes/view.js';
+import sharedState from '../../js/classes/shared-state.js';
 
 
 describe('View', () => {
@@ -14,10 +15,6 @@ describe('View', () => {
         mockElement.appendChild(cardEl);
 
         view = new View({ element: mockElement});
-
-        view.postRender = vi.fn();
-        view.applyEvents = vi.fn();
-        view.getHtml = vi.fn().mockReturnValue('<div>Mock HTML</div>');
     });
 
     it('queue element', () => {
@@ -31,4 +28,108 @@ describe('View', () => {
 
         expect(els).length(1);
     });
+
+    it('render', () => {
+        view.getHtml = vi.fn().mockReturnValue('<div class="bar">Mock HTML</div>');
+        view.render();
+
+        const els = view.queueAll('.bar');
+
+        expect(els).length(1);
+    });
+
+    it('render debug', () => {
+        sharedState.setProp('debug', true);
+        view.getHtml = vi.fn().mockReturnValue('<div class="bar">Mock HTML</div>');
+        view.render();
+
+        const els = view.queueAll('.bar');
+
+        expect(els).length(1);
+    });
+
+    it('getHtml', () => {
+        const html = view.getHtml();
+
+        expect(html).toContain('data-view-id');
+        expect(html).toContain(view.getUid());
+    });
+
+    it('destroy', () => {
+        view.cleanup = vi.fn();
+        view.destroy();
+
+        expect(view.cleanup).toHaveBeenCalled();
+        expect(view.eventSubscribersMap).toBeNull();
+    });
+
+    it('cleanup', () => {
+        view.unsubscribeAllEvents = vi.fn();
+        view.cleanup();
+
+        expect(view.unsubscribeAllEvents).toHaveBeenCalled();
+    });
+
+    it('subscribeElementEvent', () => {
+        const mockSubscriber = vi.fn();
+        const mockElement = document.createElement('div');
+
+        view.subscribeElementEvent(mockElement, 'click', mockSubscriber);
+
+        expect(view.eventSubscribersMap['click']).toHaveLength(1);
+        expect(view.eventSubscribersMap['click'][0].element).toBe(mockElement);
+        expect(view.eventSubscribersMap['click'][0].subscriber).toBe(mockSubscriber);
+    });
+
+    it('subscribeElementEvent no map', () => {
+        const mockSubscriber = vi.fn();
+        const mockElement = document.createElement('div');
+
+        view.eventSubscribersMap = null;
+        view.subscribeElementEvent(mockElement, 'click', mockSubscriber);
+
+        expect(view.eventSubscribersMap['click']).toHaveLength(1);
+        expect(view.eventSubscribersMap['click'][0].element).toBe(mockElement);
+        expect(view.eventSubscribersMap['click'][0].subscriber).toBe(mockSubscriber);
+    });
+
+    it('subscribeElementEvent no element', () => {
+        const mockSubscriber = vi.fn();
+
+        view.subscribeElementEvent(null, 'click', mockSubscriber);
+
+        expect(view.eventSubscribersMap).toEqual({});
+    });
+
+    it('subscribeElementEvent no subscriber', () => {
+        const mockElement = document.createElement('div');
+
+        view.subscribeElementEvent(mockElement, 'click', null);
+
+        expect(view.eventSubscribersMap).toEqual({});
+    });
+
+    it('unsubscribeAllEvents', () => {
+        const mockElement = document.createElement('div');
+        const mockSubscriber = vi.fn();
+
+        view.subscribeElementEvent(mockElement, 'click', mockSubscriber);
+        view.unsubscribeAllEvents();
+
+        expect(view.eventSubscribersMap).toEqual({});
+    });
+
+    it('unsubscribeAllEvents no map', () => {
+        const mockElement = document.createElement('div');
+        const mockSubscriber = vi.fn();
+
+        view.subscribeElementEvent(mockElement, 'click', mockSubscriber);
+
+        view.eventSubscribersMap = null;
+
+        view.unsubscribeAllEvents();
+
+        expect(view.eventSubscribersMap).toEqual(null);
+    });
+
 });
